f5f7543dae2bea79d7f7c644d2e6e0584dae252a
lizaelias <elias221624@gmail.com>
2023-08-22 13:10:52 +0600

    calculation js file withdraw section

--- deposit.js
+++ deposit.js
@@ -32,6 +32,37 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
 })
 
 
+document.getElementById('btn-withdraw').addEventListener('click', function () {
+    // step 2
+    const withdrawValueFiled = document.getElementById('withdraw-filed');
+    const withdrawValue = withdrawValueFiled.value;
+    const newWithdrawAmount = parseFloat(withdrawValue);
+    // step 3
+
+    withdrawValueFiled.value = ' ';
+
+    // step 4;
+
+    const withdrawElement = document.getElementById('total-withdraw');
+    const newWithdrawAmountString = withdrawElement.innerText;
+    const previousWithdraw = parseFloat(newWithdrawAmountString);
+    
+    // step 5
+    const newTotalWithdraw = previousWithdraw + newWithdrawAmount;
+
+    withdrawElement.innerText = newTotalWithdraw;
+
+
+    const previousTotalBalance = document.getElementById('Balance-total');
+    const previousTotalBalanceString = previousTotalBalance.innerText;
+    const totalBalance = parseFloat(previousTotalBalanceString);
+    
+    const currentBalance = totalBalance - newTotalWithdraw;
+
+    previousTotalBalance.innerText = currentBalance;
+ 
+})
+
 
 
 
